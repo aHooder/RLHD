99a42ff8913dd78cba9eab8b2d1abdda46a933d0
Hooder <ahooder@protonmail.com>
2024-04-16 04:25:47 +0200

    Make blood and bile flat

--- src/main/resources/rs117/hd/utils/hooder_water.glsl
+++ src/main/resources/rs117/hd/utils/hooder_water.glsl
@@ -536,8 +536,8 @@ vec4 sampleWater(int waterTypeIndex, vec3 viewDir) {
     }
 
     // If the water is opaque, blend in a fake underwater surface
-    // TODO: make abyssal bile flat, and maybe change hard-coded depth to per environment, tile or water type
-    if (waterType.isFlat || !waterTransparency || waterTypeIndex == WATER_TYPE_ABYSS_BILE) {
+    // TODO: maybe change hard-coded depth to per environment, tile or water type
+    if (waterType.isFlat || !waterTransparency) {
         // Computed from packedHslToSrgb(6676)
         const vec3 underwaterColor = vec3(0.04856183, 0.025971446, 0.005794384);
         int depth = 600;
